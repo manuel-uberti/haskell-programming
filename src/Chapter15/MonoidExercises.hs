d081cdbe5d6e7ed4391ce195b0e59e340bf7b0f0
Manuel Uberti <manuel.uberti@inventati.org>
2019-06-19 15:40:17 +0200

    Add first Monoid exercises

---
+++ src/Chapter15/MonoidExercises.hs
@@ -0,0 +1,85 @@
+module Chapter15.MonoidExercises where
+
+import Data.Semigroup
+import Test.QuickCheck
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Semigroup m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Semigroup m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+-- 1
+data Trivial =
+  Trivial
+  deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  (<>) _ _ = Trivial
+
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+-- 2
+newtype Identity a =
+  Identity a
+  deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Identity a) where
+  (<>) (Identity a) (Identity b) = Identity (a <> b)
+
+instance (Semigroup a, Monoid a) => Monoid (Identity a) where
+  mempty = Identity mempty
+  mappend = (<>)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    a <- arbitrary
+    return (Identity a)
+
+type IdentityAssoc
+   = Identity String -> Identity String -> Identity String -> Bool
+
+-- 3
+data Two a b =
+  Two a
+      b
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (<>) (Two a b) (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Semigroup a, Monoid a, Semigroup b, Monoid b) =>
+         Monoid (Two a b) where
+  mempty = Two mempty mempty
+  mappend = (<>)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Two a b)
+
+type TwoAssoc
+   = Two String String -> Two String String -> Two String String -> Bool
+
+main :: IO ()
+main = do
+  quickCheck (semigroupAssoc :: TrivAssoc)
+  quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+  quickCheck (monoidRightIdentity :: Trivial -> Bool)
+  quickCheck (semigroupAssoc :: IdentityAssoc)
+  quickCheck (monoidLeftIdentity :: Identity String -> Bool)
+  quickCheck (monoidRightIdentity :: Identity String -> Bool)
+  quickCheck (semigroupAssoc :: TwoAssoc)
+  quickCheck (monoidLeftIdentity :: Two String String -> Bool)
+  quickCheck (monoidRightIdentity :: Two String String -> Bool)
